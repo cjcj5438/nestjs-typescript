2a17ad4cb7e1891b5701ff8b18fa46ea457d8395
cjcj5438 <1002931123@qq.com>
2019-10-10 11:34:42 +0800

    添加用户user

---
+++ src/modules/user/user.entity.ts
@@ -0,0 +1,31 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, BeforeInsert, BeforeUpdate } from 'typeorm';
+import { Exclude } from 'class-transformer';
+import * as bcrypt from 'bcrypt';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  @Exclude()// 过滤掉密码返回
+  password: string;
+
+  @CreateDateColumn()
+  created: Date;
+
+  @UpdateDateColumn()
+  updated: Date;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 12);
+  }
+  async comparePassword(password: string) {
+    return await bcrypt.compare(password, this.password);
+  }
+}
